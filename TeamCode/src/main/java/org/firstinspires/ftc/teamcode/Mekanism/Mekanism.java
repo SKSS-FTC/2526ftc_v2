f3dd5fdc437edae5f868e6d247ef4fdfe03a79ec
Colin M. <ccmcc2005@gmail.com>
2024-11-08 19:32:05 -0600

    11/8/24
    More editing, and revsing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mekanism/Mekanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mekanism/Mekanism.java
@@ -1,94 +1,108 @@
 // Copyright (c) 2024-2025 FTC 13532
 // All rights reserved.
 
+
 package org.firstinspires.ftc.teamcode.Mekanism;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
+/**
+ * This class is for the mechanism
+ */
 public class Mekanism {
 
-  public final LinearOpMode myOp;
-
-  public Mekanism(LinearOpMode opMode) {
-    myOp = opMode;
-  }
-
-  DcMotor pivot, slide, spintake;
-  Servo claw, wrist;
-
-  int COUNTS_PER_INCH = 1120;
-  int COUNTS_PER_DEGREE = 1120;
-
-  /**
-   * Initializes the mechanism of the robot.<br>
-   * Starts all the devices and maps where they go As well as sets direction and whether motors run
-   * with encoders or not
-   */
-  public void initMekanism() {
-
-    /*
-    // Init slaw, claw, and pivot
-    pivot = myOp.hardwareMap.get(DcMotor.class, "pivot");
-    slide = myOp.hardwareMap.get(DcMotor.class, "slide");
+    // Allows calling of LinearOpMode functions
+    public final LinearOpMode myOp;
 
 
-    pivot.setTargetPosition(0);
-    slide.setTargetPosition(0);
+    DcMotor pivot, slide;
+    Servo spintake, wrist;
 
-    pivot.setDirection(DcMotor.Direction.FORWARD);
-    slide.setDirection(DcMotor.Direction.FORWARD);
+    // TODO: Find proper numbers for this
+    int COUNTS_PER_INCH = 1120; // Encoder counts per inch slide movement
+    int COUNTS_PER_DEGREE = 1120; // Encoder counts per degree
 
-    pivot.setMode(STOP_AND_RESET_ENCODER);
-    slide.setMode(STOP_AND_RESET_ENCODER);
 
-    pivot.setMode(RUN_USING_ENCODER);
-    slide.setMode(RUN_USING_ENCODER);
+    // Constructor for Mekanism stuff
+    public Mekanism(LinearOpMode myOp) {
+        this.myOp = myOp;
 
-    pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // Init slaw, claw, and pivot
+        pivot = myOp.hardwareMap.get(DcMotor.class, "pivot");
+        slide = myOp.hardwareMap.get(DcMotor.class, "slide");
 
-    // Sets maximum allowed power to 1
-    pivot.setPower(1);
-    slide.setPower(1);
+        // Initial target of the motors
+        pivot.setTargetPosition(0);
+        slide.setTargetPosition(0);
 
+        // Directions
+        // TODO: Set proper directions
+        pivot.setDirection(DcMotor.Direction.FORWARD);
+        slide.setDirection(DcMotor.Direction.FORWARD);
+
+        pivot.setMode(STOP_AND_RESET_ENCODER);
+        slide.setMode(STOP_AND_RESET_ENCODER);
+
+        pivot.setMode(RUN_USING_ENCODER);
+        slide.setMode(RUN_USING_ENCODER);
+
+        pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Sets maximum allowed power to 1
+        pivot.setPower(1);
+        slide.setPower(1);
+
+
+        // Servos for the end effector
+        wrist = myOp.hardwareMap.get(Servo.class, "wrist");
+        spintake = myOp.hardwareMap.get(Servo.class, "spintake"); // TODO: Get proper name
+        wrist.scaleRange(0, 1); // TODO: Set ranges for wrist positions
+    }
+
+    /**
+     * Set the position and angle for arm independently
      */
+    public void basicMoveArm(int position, int angle) {
 
-    wrist = myOp.hardwareMap.get(Servo.class, "wrist");
-    claw = myOp.hardwareMap.get(Servo.class, "claw");
-    claw.scaleRange(0, 1);
-  }
-
-  // set the pos and angle for arm independently
-  public void basicMoveArm(int position, int angle) {
-
-    slide.setTargetPosition(position);
-    pivot.setTargetPosition(angle);
-  }
-
-  // In IN.
-  // uses x and y to calculate arm angle and length
-  public void moveXY(int x, int y) {
-
-    if (x < 0) {
-      x = 0;
-    }
-    if (y < 0) {
-      y = 0;
+        slide.setTargetPosition(position);
+        pivot.setTargetPosition(angle);
     }
 
-    int armLen = (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    /**
+     * In IN.
+     * Uses x and y to calculate arm angle and length
+     */
+    public void moveXY(int x, int y) {
 
-    int armAngle = (int) Math.atan((double) y / (double) x);
+        if (x < 0) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = 0;
+        }
 
-    slide.setTargetPosition(armLen * COUNTS_PER_INCH);
+        int armLen = (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 
-    pivot.setTargetPosition(armAngle * COUNTS_PER_DEGREE);
-  }
+        int armAngle = (int) Math.atan((double) y / (double) x);
 
-  // sets the pos for claw
-  public void moveClaw(double position) {
-    claw.setPosition(position);
-  }
+        slide.setTargetPosition(armLen * COUNTS_PER_INCH);
+
+        pivot.setTargetPosition(armAngle * COUNTS_PER_DEGREE);
+    }
+
+    /**
+     * Sets the position of the claw
+     *
+     * @param position Input
+     */
+    public void moveClaw(double position) {
+        spintake.setPosition(position);
+    }
 }
