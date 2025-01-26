fadc1e867d1750a357d1231909559cb32095bde7
Alexking11 <dillonqgardner@gmail.com>
2025-01-26 13:56:37 -0800

    Tried to attach the color picker to the I2C device.

--- TeamCode/src/main/java/org/nknsd/teamcode/programs/tests/VisionSensorTester.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/programs/tests/VisionSensorTester.java
@@ -3,6 +3,10 @@ package org.nknsd.teamcode.programs.tests;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.nknsd.teamcode.components.utility.ColorPicker;
+import org.nknsd.teamcode.components.utility.GamePadHandler;
+import org.nknsd.teamcode.controlSchemes.abstracts.Generic2PControlScheme;
+import org.nknsd.teamcode.controlSchemes.reals.KarstenGeneric2PController;
 import org.nknsd.teamcode.frameworks.NKNComponent;
 import org.nknsd.teamcode.components.sensors.hummelvision.LilyVisionHandler;
 import org.nknsd.teamcode.frameworks.NKNProgramTrue;
@@ -14,8 +18,21 @@ import java.util.List;
 public class VisionSensorTester extends NKNProgramTrue {
     @Override
     public void createComponents(List<NKNComponent> components, List<NKNComponent> telemetryEnabled) {
+        GamePadHandler gamePadHandler = new GamePadHandler();
+        components.add(gamePadHandler);
+
+        ColorPicker colorPicker = new ColorPicker();
+        components.add(colorPicker);
+        telemetryEnabled.add(colorPicker);
+
         LilyVisionHandler lilyVisionHandler = new LilyVisionHandler();
         components.add(lilyVisionHandler);
         telemetryEnabled.add(lilyVisionHandler);
+
+        KarstenGeneric2PController controller = new KarstenGeneric2PController();
+        controller.link(gamePadHandler);
+
+        colorPicker.link(gamePadHandler, controller);
+        lilyVisionHandler.link(colorPicker);
     }
 }
