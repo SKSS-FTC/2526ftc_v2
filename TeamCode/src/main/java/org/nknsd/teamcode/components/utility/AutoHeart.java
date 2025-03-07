9744188f2ce69b977405aeaf23ae0c1d1e1f0357
Alexking11 <dillonqgardner@gmail.com>
2025-03-06 17:55:22 -0800

    no message

--- TeamCode/src/main/java/org/nknsd/teamcode/components/utility/AutoHeart.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/utility/AutoHeart.java
@@ -6,12 +6,19 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.nknsd.teamcode.frameworks.NKNComponent;
+import org.nknsd.teamcode.frameworks.NKNStep;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class AutoHeart implements NKNComponent {
+    private NKNStep[] nknSteps;
+    private HashMap<String, NKNStep> activeSteps;
 
     @Override
     public boolean init(Telemetry telemetry, HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2) {
-        return false;
+        return true;
     }
 
     @Override
@@ -31,16 +38,41 @@ public class AutoHeart implements NKNComponent {
 
     @Override
     public String getName() {
-        return null;
+        return "AutoHeart";
     }
 
     @Override
     public void loop(ElapsedTime runtime, Telemetry telemetry) {
+        if (activeSteps.isEmpty()) {
+            return;
+        }
 
+        for (NKNStep step : activeSteps.values()) {
+            step.main();
+        }
+    }
+
+    public void addStep(NKNStep step) {
+        // Make sure the step doesn't already exist first!
+        if (activeSteps.containsValue(step)) {
+            activeSteps.put(step.name, step);
+        }
+    }
+
+    public void removeStep(NKNStep step) {
+        activeSteps.remove(step.name);
+    }
+
+    public void end() {
+        activeSteps.clear();
     }
 
     @Override
     public void doTelemetry(Telemetry telemetry) {
 
+    }
+
+    public void link(NKNStep startStep){
+        activeSteps.put(startStep.name, startStep);
     }
 }
--- TeamCode/src/main/java/org/nknsd/teamcode/frameworks/NKNStep.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/frameworks/NKNStep.java
@@ -1,20 +1,68 @@
 package org.nknsd.teamcode.frameworks;
 
+import org.nknsd.teamcode.components.utility.AutoHeart;
+
+import java.util.ArrayList;
+
 public abstract class NKNStep {
-    private NKNStep nknStep;
+    public String name;
+    private ArrayList<NKNStep> parentSteps, childSteps;
+    private AutoHeart autoHeart;
 
-    public abstract void doTask();
-
-    public NKNStep end() {
-        if (doneTask()) {
-            return nknStep;
+    private boolean shouldStart() {
+        for (NKNStep parent : parentSteps) {
+            if (!parent.doneTask()) {
+                return false;
+            }
         }
-        return null;
+
+        return true;
     }
 
-    protected abstract boolean doneTask();
+    private boolean hasStarted = false;
+    public void main() {
+        if (!hasStarted) {
+            if (shouldStart()) {
+                hasStarted = true;
+                startUp();
 
-    public void link(NKNStep nknStep) {
-        this.nknStep = nknStep;
+            } else {
+                return;
+
+            }
+        }
+
+        doTask();
+
+        // On completion, create children
+        if (doneTask()) {
+            for(NKNStep child : childSteps){
+               autoHeart.addStep(child);
+
+            }
+
+        }
+    }
+
+    protected abstract void startUp();
+    protected abstract void doTask();
+    public abstract boolean doneTask();
+
+    protected void killParents() {
+        for (NKNStep parent : parentSteps) {
+            autoHeart.removeStep(parent);
+        }
+    }
+
+    public void link(AutoHeart autoHeart) {
+        this.autoHeart = autoHeart;
+    }
+
+    public void addChild(NKNStep child) {
+        childSteps.add(child);
+    }
+
+    public void addParent(NKNStep parent) {
+        parentSteps.add(parent);
     }
 }
