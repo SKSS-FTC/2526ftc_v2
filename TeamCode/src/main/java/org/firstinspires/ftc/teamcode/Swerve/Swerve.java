dcbcc9a3226cbb80113850fc188d001a6d63034b
EagleNotBot <phibersto26@gmail.com>
2025-06-17 20:01:49 -0500

    stuff
    Might want to remove odo from main class
    Controller input finalized 0/360 in front 90 at right, 180 back, and 270 left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/Swerve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/Swerve.java
@@ -19,17 +19,23 @@ public class Swerve {
   public Swerve(LinearOpMode opMode, GoBildaPinpointDriver odo) {
     this.opMode = opMode;
     telemetry = opMode.telemetry;
+    // might change odo to keep it isolated in here that way there aren't conflicting odo's due to odo being in pivot as well
     this.odo = odo;
   }
 
   public void drive(double x, double y, double rotate) {
     odo.update();
+
     double vector_ang = Math.atan2(x, y) * (180 / Math.PI);
+    if(vector_ang < 0)
+      vector_ang = 360 + vector_ang;
     double vector_pow = Math.sqrt((x * x + y * y));
     telemetry.addLine("input vector angle: " + vector_ang);
     telemetry.addLine("input vector power: " + vector_pow);
     vector_ang = (vector_ang - 90) % 360;
     vector_ang /= 360;
     telemetry.addLine("input vector angle: " + vector_ang);
+
+    //0/360 in front, 90 at right, 180 back, and 270 left
   }
 }
