4ac9f37f2e2dad12dcdb051810b2c3dd2e37c080
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-09-09 21:23:30 -0400

    Basic Intake setup! Note: The intaking power is set to 0, do not be alarmed when it doesn't move.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
+
+    // ARC Thunder Vortex
+
+    private final double intakingPower = 0.0;
+    private MotorEx intakeMotor;
+    private boolean running;
+
+    public Intake(HardwareMap hardwareMap)
+    {
+        intakeMotor = new MotorEx(hardwareMap, "intake");
+    }
+
+    public void run(boolean startMotor)
+    {
+        running = startMotor;
+        if(startMotor)
+        {
+            intakeMotor.set(intakingPower);
+        }
+        else {
+            intakeMotor.stopMotor();
+        }
+    }
+
+    public boolean isRunning()
+    {
+        return running;
+    }
 }
