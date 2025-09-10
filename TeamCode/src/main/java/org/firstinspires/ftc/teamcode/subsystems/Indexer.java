61e9035992b72948bb7e2937cc2cb82890996c9f
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-09-10 11:47:39 -0400

    moveTo function with efficiency algorithms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Indexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Indexer.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Indexer {
 
+    //TODO: Optimize the algorithms, it'll work as is though
+
     // 0, 120, 240, 360 are the possible angles
     // since 0 and 360 are the same ball, no matter where you are,
     // you can "quick spin" to shoot all 3 balls quickly
@@ -14,6 +16,9 @@ public class Indexer {
     private final int ANGLE_TWO = 120;
     private final int ANGLE_THREE = 240;
     private final int ANGLE_ALT = 360;
+
+
+    private IndexerState state;
 
     public enum IndexerState
     {
@@ -28,7 +33,30 @@ public class Indexer {
     public Indexer (HardwareMap hardwareMap)
     {
         indexerServo = new SimpleServo(hardwareMap, "index",0,360);
+    }
 
+    public void moveTo(int toPos)
+    {
+        switch(toPos){
+            case 0:
+                if(state == IndexerState.two) {
+                    indexerServo.turnToAngle(ANGLE_ONE);
+                    state = IndexerState.one;
+                }
+                if(state == IndexerState.three) {
+                    indexerServo.turnToAngle(ANGLE_ALT);
+                    state = IndexerState.oneAlt;
+                }
+                break;
+            case 1:
+                indexerServo.turnToAngle(ANGLE_TWO);
+                state = IndexerState.two;
+                break;
+            case 2:
+                indexerServo.turnToAngle(ANGLE_THREE);
+                state = IndexerState.three;
+                break;
+        }
     }
 
 }
