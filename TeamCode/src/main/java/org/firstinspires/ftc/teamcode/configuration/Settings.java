65bc8fc5c6b67d2c695e9380f4679445d836015e
Ben Boonstra <bboonstra26@gmail.com>
2025-09-12 19:56:35 -0500

    Refactor hardware mappings to use constants from Settings for pinpoint and limelight
    Closes #5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configuration/Settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configuration/Settings.java
@@ -91,6 +91,7 @@ public class Settings {
             // Drive motors
             public static final String FRONT_LEFT_MOTOR = "frontLeft";
             public static final String FRONT_RIGHT_MOTOR = "frontRight";
+            public static final String PINPOINT = "pinpoint";
             public static final String REAR_LEFT_MOTOR = "rearLeft";
             public static final String REAR_RIGHT_MOTOR = "rearRight";
 
