e482fe79ae73d6f933995522d99ee01b0809feb2
CowLORD420 <mateijenaru123@gmail.com>
2025-09-18 11:11:27 +0300

    am schimbat sa dea return Imu doar la heading ca oricum fac kalman filter (:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IMU.java
@@ -2,10 +2,8 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 public class IMU {
     private final com.qualcomm.robotcore.hardware.IMU imu;// radians
-    private double lastRawHeading = Double.NaN;
-    private double cosHeading = 1.0;          // cos(0)
-    private double sinHeading = 0.0;          // sin(0)
-    private static final double EPSILON = 1e-6;
+    private double lastRawHeading = Double.NaN;     // sin(0)
+    private static final double EPSILON = 1e-5;
 
     public IMU(com.qualcomm.robotcore.hardware.IMU imu){
         this.imu = imu;
@@ -16,26 +14,14 @@ public class IMU {
         double rawHeading = Math.toRadians(imu.getRobotYawPitchRollAngles().getYaw());
 
         if (Double.isNaN(lastRawHeading) || Math.abs(rawHeading - lastRawHeading) > EPSILON) {
-            cosHeading = Math.cos(rawHeading);
-            sinHeading = Math.sin(rawHeading);
             lastRawHeading = rawHeading;
         }
     }
 
-    public double getCos() {
-        updateCachedHeading();
-        return cosHeading;
-    }
-
-    public double getSin() {
-        updateCachedHeading();
-        return sinHeading;
-    }
+    public double getLastRawHeading(){ return lastRawHeading;}
 
     public void resetCache() {
         lastRawHeading = Double.NaN;
-        cosHeading = 1.0;
-        sinHeading = 0.0;
     }
 
 }
