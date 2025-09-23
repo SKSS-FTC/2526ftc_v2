e172fa1248d50bdb24e4069507bdaa15ea45d06e
tango8 <ethan.b.tang@gmail.com>
2025-09-22 20:03:31 -0400

    Apriltags have their own camera pipeline, camera is not needed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.testing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.Camera;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Outtake;
 
