35b38a8491ee40e4f30345d3717045da67d6b945
Ben Boonstra <bboonstra26@gmail.com>
2025-09-23 21:58:34 -0500

    NW Fest Chassis OpMode
    Signed-off-by: Ben Boonstra <bboonstra26@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach/NorthwestFest.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.outreach;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Main TeleOp class for driver-controlled period.
+ * Handles controller profile selection and robot operation during matches.
+ *
+ * @noinspection ClassWithoutConstructor
+ */
+@TeleOp(name = "Outreach", group = "Outreach")
+public class NorthwestFest extends OpMode {
+	public DcMotor frontLeftMotor;
+	public DcMotor frontRightMotor;
+	public DcMotor backLeftMotor;
+	public DcMotor backRightMotor;
+	
+	
+	@Override
+	public final void init() {
+		frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+		frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+		backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+		backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+		// TODO reverse motors n stuff
+		frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+		backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+	}
+	
+	
+	public final void loop() {
+		double drivePower = -gamepad1.left_stick_y;
+		double strafePower = gamepad1.left_stick_x;
+		double rotatePower = gamepad1.right_stick_x;
+		
+		if (gamepad1.right_bumper) {
+			rotatePower = -0.5;
+		}
+		if (gamepad1.left_bumper) {
+			rotatePower = 0.5;
+		}
+		if (gamepad1.dpad_up) {
+			drivePower = 0.5;
+		}
+		if (gamepad1.dpad_down) {
+			drivePower = -0.5;
+		}
+		if (gamepad1.dpad_left) {
+			strafePower = -0.5;
+		}
+		if (gamepad1.dpad_right) {
+			strafePower = 0.5;
+		}
+		
+		mecanumDrive(strafePower, drivePower, rotatePower);
+	}
+	
+	public void mecanumDrive(double strafePower, double drivePower, double rotatePower) {
+		double frontLeftPower = drivePower + strafePower + rotatePower;
+		double frontRightPower = drivePower - strafePower - rotatePower;
+		double backLeftPower = drivePower - strafePower + rotatePower;
+		double backRightPower = drivePower + strafePower - rotatePower;
+		
+		double maxPower = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+		maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+		maxPower = Math.max(maxPower, Math.abs(backRightPower));
+		
+		if (maxPower > 1.0) {
+			frontLeftPower /= maxPower;
+			frontRightPower /= maxPower;
+			backLeftPower /= maxPower;
+			backRightPower /= maxPower;
+		}
+		
+		frontLeftMotor.setPower(frontLeftPower);
+		frontRightMotor.setPower(frontRightPower);
+		backLeftMotor.setPower(backLeftPower);
+		backRightMotor.setPower(backRightPower);
+	}
+}
