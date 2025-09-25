517730ec93bf63a048e307d28f3591f87a860697
baqwas <baqwas@gmail.com>
2025-09-24 23:21:43 -0500

    TeleOpPreviewEvent - second pass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holonomic/MecanumRotateIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holonomic/MecanumRotateIMU.java
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Holonomic;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
