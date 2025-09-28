0c3013cad7647048effdd0cb57cdf35b3dae7546
anivikr21 <anivikr@gmail.com>
2025-09-28 14:11:12 -0700

    Added variable for the speed of the motors that will run during the first couple seconds of auto to leave the launch zone.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
@@ -4,5 +4,6 @@ public class AutoConstants {
     static final double DRIVE_SPEED = 0.5;
     static final double TURN_SPEED = 0.5;
     static final double STRAFE_SPEED = 0.5;
+    static final double LAUNCH_EXIT_SPEED = 0.8;
 
 }
