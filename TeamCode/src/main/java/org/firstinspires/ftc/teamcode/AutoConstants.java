9f2791c29b9b7ca7fda841ae162a1f9d70b57f73
anivikr21 <anivikr@gmail.com>
2025-09-28 13:58:41 -0700

    Empty Auto Classes, subsystem classes must be added, example code placed in AutoConstants.java,

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+
+
+public class AutoConstants {
+    static final double DRIVE_SPEED = 0.5;
+    static final double TURN_SPEED = 0.5;
+    static final double STRAFE_SPEED = 0.5;
+
+
+
+
+
+
+}
