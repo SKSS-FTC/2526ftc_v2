2c4ce8c8289653d2535f2e314915c30a6549d3df
anivikr21 <anivikr@gmail.com>
2025-10-01 09:53:02 -0700

    Added Movement to autonomous, should move to the Classifier and prepare to shoot, we will need to add in the shooter code when the sub systems/classes are built. I added example code to what it should look like.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeLM1Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeLM1Auto.java
@@ -10,6 +10,7 @@
     import com.qualcomm.robotcore.hardware.IMU;
     import com.qualcomm.robotcore.util.ElapsedTime;
 
+    import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
     import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
     @Autonomous(name = "Test")
@@ -18,12 +19,20 @@ public class DecodeLM1Auto extends LinearOpMode {
     GoBildaPinpointDriver odo;
     DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
         int counter = 0;
+
+        IMU imu;
     @Override
     public void runOpMode() {
 
         initAuto();
 
-
+            driveToPos(16, 23);
+            gyroTurnToAngle(110);
+        //TODO: When the robot classes get built we need to add actions for outake to score the pre-load
+//      Example  Code:
+//      DecodeAutoArtifactLaunch;
+//      driveToPos(40, 23);
+/*
         while (counter < 2) {
             frontLeftMotor.setPower(AutoConstants.DRIVE_SPEED);
             backLeftMotor.setPower(AutoConstants.DRIVE_SPEED);
@@ -34,15 +43,118 @@ public class DecodeLM1Auto extends LinearOpMode {
         backLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backRightMotor.setPower(0);
-
-
-
+*/
 
 
 
     }
 
-    private void initAuto() {
+        private void driveToPos(double targetX, double targetY) {
+            odo.update();
+            boolean telemAdded = false;
+
+            while (opModeIsActive() &&
+                    (Math.abs(targetX - odo.getPosX(DistanceUnit.CM)) > 30 || Math.abs(targetY - odo.getPosY(DistanceUnit.CM)) > 30)
+            ){
+                odo.update();
+
+                double x = 0.001*(targetX - odo.getPosX(DistanceUnit.CM));
+                double y = -0.001*(targetY - odo.getPosY(DistanceUnit.CM));
+
+                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS); // getRobotOrientationAsQuaternion().  .().firstAngle(); //.getHeading();
+
+                double rotY = y * Math.cos(-botHeading) - x * Math.sin(-botHeading);
+                double rotX = y * Math.sin(-botHeading) + x * Math.cos(-botHeading);
+
+                if (!telemAdded) {
+                    telemetry.addData("x: ", x);
+                    telemetry.addData("y: ", y);
+                    telemetry.addData("rotX: ", rotX);
+                    telemetry.addData("rotY: ", rotY);
+                    telemetry.update();
+                    telemAdded = true;
+                }
+
+                if (Math.abs(rotX) < 0.15) {
+                    rotX = Math.signum(rotX) * 0.15;
+                }
+
+                if (Math.abs(rotY) < 0.15) {
+                    rotY = Math.signum(rotY) * 0.15;
+                }
+
+                double denominator = Math.max(Math.abs(y) + Math.abs(x), 1);
+                double frontLeftPower = (rotX + rotY)
+                        / denominator;
+                double backLeftPower = (rotX - rotY) / denominator;
+                double frontRightPower = (rotX - rotY) / denominator;
+                double backRightPower = (rotX + rotY) / denominator;
+
+                frontLeftMotor.setPower(frontLeftPower);
+                backLeftMotor.setPower(backLeftPower);
+                frontRightMotor.setPower(frontRightPower);
+                backRightMotor.setPower(backRightPower);
+
+/*                telemetry.addData("X: ", odo.getPosX());
+                telemetry.addData("Y: ", odo.getPosY());
+                telemetry.addData("Heading Odo: ", Math.toDegrees(odo.getHeading()));
+                telemetry.addData("Heading IMU: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+*/                telemetry.update();
+            }
+
+            frontLeftMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backRightMotor.setPower(0);
+        }
+
+
+        private void gyroTurnToAngle(double turnAngle) {
+            double error, currentHeadingAngle, driveMotorsPower;
+            imu.resetYaw();
+
+            error = turnAngle;
+
+            while (opModeIsActive() && ((error > 1) || (error < -1))) {
+                odo.update();
+                telemetry.addData("X: ", odo.getPosX(DistanceUnit.CM));
+                telemetry.addData("Y: ", odo.getPosY(DistanceUnit.CM));
+//                telemetry.addData("Heading Odo: ", Math.toDegrees(odo.getHeading()));
+                telemetry.addData("Heading IMU: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+                telemetry.update();
+
+                /*driveMotorsPower = error / 200;
+
+                if ((driveMotorsPower < 0.2) && (driveMotorsPower > 0)) {
+                    driveMotorsPower = 0.2;
+                } else if ((driveMotorsPower > -0.2) && (driveMotorsPower < 0)) {
+                    driveMotorsPower = -0.2;
+                }*/
+                driveMotorsPower = error / 50;
+
+                if ((driveMotorsPower < 0.35) && (driveMotorsPower > 0)) {
+                    driveMotorsPower = 0.35;
+                } else if ((driveMotorsPower > -0.35) && (driveMotorsPower < 0)) {
+                    driveMotorsPower = -0.35;
+                }
+                // Positive power causes left turn
+                frontLeftMotor.setPower(-driveMotorsPower);
+                backLeftMotor.setPower(-driveMotorsPower);
+                frontRightMotor.setPower(driveMotorsPower);
+                backRightMotor.setPower(driveMotorsPower);
+
+                currentHeadingAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+                error = turnAngle - currentHeadingAngle;
+            }
+            frontLeftMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backRightMotor.setPower(0);
+
+
+        }
+
+        private void initAuto() {
         odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
         //        odo.setOffsets(101.6, 95.25 ); //these are tuned for 3110-0002-0001 Product Insight #1
         odo.setOffsets(107.95, 95.25, DistanceUnit.CM ); //took on 12/20 by Rohan
