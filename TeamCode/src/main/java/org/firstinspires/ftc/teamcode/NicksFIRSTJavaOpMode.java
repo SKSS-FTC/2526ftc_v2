1854c6ebbfca5189cc16d5ddca1d8e1cbcdbabe4
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-10-01 11:20:19 -0500

    Mecanum configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NicksFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NicksFIRSTJavaOpMode.java
@@ -2,17 +2,47 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Nick's OpMode", group="Example OpMode")
 public class NicksFIRSTJavaOpMode extends OpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
     @Override
     public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightDrive");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         telemetry.addData("Status", "Initialised");
         telemetry.update();
     }
 
     @Override
     public void loop() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+
         telemetry.addData("Status", "Running");
         telemetry.update();
     }
