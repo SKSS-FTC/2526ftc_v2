57e57b84a7cca35dff5249dee778c79615fae550
KMP3344 <dragonbreath1212@outlook.com>
2025-10-02 15:12:31 -0700

    Remove unused Telemetry import in DriveTrain
    Deleted an unused import of Telemetry from DriveTrain.java to clean up the code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class DriveTrain {
