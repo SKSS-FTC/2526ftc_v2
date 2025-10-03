d78fea8ade0181dcb189d2519713401c9eec66ef
dysshanks <ryanvdvorst@outlook.com>
2025-10-03 13:53:24 +0200

    added all methods for the modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-@Autonomous
-public class autonomousMode extends OpMode
+@TeleOp
+public class teleOpMode extends OpMode
 {
     @Override
     public void init()
