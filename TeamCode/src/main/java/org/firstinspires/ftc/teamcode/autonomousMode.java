586ced8e19a7410063d3953bd50ae60413cefa86
dysshanks <ryanvdvorst@outlook.com>
2025-10-03 13:20:36 +0200

    added autonomous and pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousMode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class autonomousMode extends OpMode
+{
+    @Override
+    public void init()
+    {
+
+    }
+    @Override
+    public void init_loop()
+    {
+
+    }
+    @Override
+    public void loop()
+    {
+
+    }
+}
