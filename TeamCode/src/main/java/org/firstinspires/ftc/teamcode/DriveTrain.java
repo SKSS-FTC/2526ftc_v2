07d59e5aa047db2c7e633db95f50662b27ec25a0
KMP3344 <dragonbreath1212@outlook.com>
2025-10-04 15:47:36 -0700

    Change setMovePower parameter type to float
    Updated the setMovePower method to accept a float instead of an int for motor power, allowing for more precise control of motor speed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -204,7 +204,7 @@ public class DriveTrain {
         backRightMotor.setPower(-power);
     }
 
-    private void setMovePower(int power) {
+    private void setMovePower(float power) {
         // Set the power for all motors
         frontLeftMotor.setPower(power);
         frontRightMotor.setPower(power);
