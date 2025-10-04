992dd379817ce076a7529d8ce2a910ed1054bcbb
KMP3344 <dragonbreath1212@outlook.com>
2025-10-04 16:09:12 -0700

    Add DriveTrainTest autonomous opmode
    Introduces DriveTrainTest, an autonomous LinearOpMode for testing the DriveTrain class by setting move power to 0.25 while active.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class DriveTrainTest extends LinearOpMode {
+    private DriveTrain driveTrain;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain = new DriveTrain(this);
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+            driveTrain.setMovePower(0.25);
+        }
+
+    }
+}
